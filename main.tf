6f86628e047560929fc9ef9d3efe503b0dcfbbda
omnibis <omnibis@MacBook-Pro.local>
2026-03-29 12:17:20 +0800

    adding initial commit

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ module "localmodule" {
   source  = "app.terraform.io/omnibis/localmodule/local"
   version = "1.0.1"
   content = "test"
+  filename = "testfile"
   # insert required variables here
 }
 
