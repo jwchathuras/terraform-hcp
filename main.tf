0eb5f46df3be043c45d34601e12a89c4ade4ab4b
omnibis <omnibis@MacBook-Pro.local>
2026-03-29 12:10:34 +0800

    adding initial commit

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-module "test_file" {
-  source   = "./modules/myfile"
-  filename = "hello.txt"
-  content  = "Hello Terraform Local Module!"
+
+module "localmodule" {
+  source  = "app.terraform.io/omnibis/localmodule/local"
+  version = "1.0.1"
+  # insert required variables here
 }
 
 output "created_file" {
-  value = module.test_file.file_path
+  value = module.localmodule.file_path
 }
